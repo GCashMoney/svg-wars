858a5e287e273108fe49a030925de3a5b78d0ebb
Gavin Macdonald <macdonaldg1@central.edu>
2016-02-16 19:22:44 -0800

    Finally got the svg image to animate!

--- functions.php
+++ functions.php
@@ -31,10 +31,10 @@
 	}
 
  //Allows svg's to be uploaded to media
-	function cc_mime_types( $mimes ){
-		$mimes['svg'] = 'image/svg+xml';
-		return $mimes;
-	}
-	add_filter( 'upload_mimes', 'cc_mime_types' );
+//	function cc_mime_types( $mimes ){
+//		$mimes['svg'] = 'image/svg+xml';
+//		return $mimes;
+//	}
+//	add_filter( 'upload_mimes', 'cc_mime_types' );
 
 	
